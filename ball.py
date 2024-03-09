f2ac52a5e6a8faee054e96d91ad43e4439b0c222
chillcoffee <ruffaresentes@me.com>
2024-03-09 09:47:47 +0800

    Create ball

---
+++ ball.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.penup()
+        self.setheading(60)
+        self.home()
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from ball import Ball
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, key="Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, key="s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    ball.forward(500)
 
 screen.exitonclick()
